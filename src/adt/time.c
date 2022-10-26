90a88ff8a3eb72c902e17f1bcfd01e96855d4ca6
Alexander Jason <alexanderjason526@gmail.com>
2022-10-26 21:53:42 +0700

    Push adt time

--- src/adt/time.c
+++ src/adt/time.c
@@ -1,116 +1,115 @@
 #include "boolean.h"
 #include "time.h"
 #include <stdio.h>
-
 /* ***************************************************************** */
 /* DEFINISI PRIMITIF                                                 */
 /* ***************************************************************** */
 /* KELOMPOK VALIDASI TERHADAP TYPE                                   */
 /* ***************************************************************** */
-boolean IsTIMEValid (int H, int M, int S) {
-/* Mengirim true jika H,M,S dapat membentuk T yang valid */
-/* dipakai untuk mentest SEBELUM membentuk sebuah Jam */
+boolean IsTIMEValid(int D, int H, int M){
     return (
+        (0 <= D && D <= 29)
+        &&
         (0 <= H && H <= 23)
         &&
         (0 <= M && M <= 59)
-        &&
-        (0 <= S && S <= 59)
     );
-}
+};
+/* Mengirim true jika H,M,S dapat membentuk T yang valid */
+/* dipakai untuk mentest SEBELUM membentuk sebuah Jam */
 
-/* *** Konstruktor: Membentuk sebuah Time dari komponen-komponennya *** */
-void CreateTime (Time * T, int HH, int MM, int SS) {
-/* Membentuk sebuah Time dari komponen-komponennya yang valid */
-/* Prekondisi : HH, MM, SS valid untuk membentuk Time */
+/* *** Konstruktor: Membentuk sebuah TIME dari komponen-komponennya *** */
+void CreateTime(Time* T, int DD, int HH, int MM){
+    Day(*T) = DD;
     Hour(*T) = HH;
     Minute(*T) = MM;
-    Second(*T) = SS;
-}
+};
+/* Membentuk sebuah TIME dari komponen-komponennya yang valid */
+/* Prekondisi : DD, HH, MM valid untuk membentuk TIME */
 
 /* ***************************************************************** */
 /* KELOMPOK BACA/TULIS                                               */
 /* ***************************************************************** */
-void BacaTIME (Time * T) {
+void BacaTIME(Time* T){
+    boolean valid;
+    do
+    {
+        int H, M, D;
+        scanf("%d %d %d", &D, &H, &M);
+        valid = IsTIMEValid(D,H, M);
+        if (!valid)
+        {
+            printf("Waktu tidak valid\n");
+        }
+        else
+        {
+            CreateTime(T, D, H, M);
+        }
+    } while (!valid);
+};
 /* I.S. : T tidak terdefinisi */
 /* F.S. : T terdefinisi dan merupakan jam yang valid */
-/* Proses : mengulangi membaca komponen HH, MM, SS sehingga membentuk T */
+/* Proses : mengulangi membaca komponen DD, HH, MM sehingga membentuk T */
 /* yang valid. Tidak mungkin menghasilkan T yang tidak valid. */
-/* Pembacaan dilakukan dengan mengetikkan komponen HH, MM, SS
+/* Pembacaan dilakukan dengan mengetikkan komponen DD, HH, MM
    dalam satu baris, masing-masing dipisahkan 1 spasi, diakhiri enter. */
-/* Jika Time tidak valid maka diberikan pesan: "Jam tidak valid", dan pembacaan
+/* Jika TIME tidak valid maka diberikan pesan: "Jam tidak valid", dan pembacaan
    diulangi hingga didapatkan jam yang valid. */
 /* Contoh:
    60 3 4
    Jam tidak valid
    1 3 4
-   --> akan terbentuk Time <1,3,4> */
-    boolean valid;
-    do
-    {
-        int H, M, S;
-        scanf("%d %d %d", &H, &M, &S);
-        valid = IsTIMEValid(H, M, S);
-        if (!valid)
-        {
-            printf("Jam tidak valid\n");
-        }
-        else
-        {
-            CreateTime(T, H, M, S);
-        }
-    } while (!valid);
+   --> akan terbentuk TIME <1,3,4> */
 
-}
-
-void TulisTIME (Time T) {
-/* I.S. : T sembarang */
-/* F.S. : Nilai T ditulis dg format HH:MM:SS */
-/* Proses : menulis nilai setiap komponen T ke layar dalam format HH:MM:SS
-   tanpa karakter apa pun di depan atau belakangnya, termasuk spasi, enter, dll.*/
+void TulisTIME(Time T){
     printf(
-        "%02d:%02d:%02d",
-        Hour(T), Minute(T), Second(T)
+        "%02d.%02d",
+        Hour(T), Minute(T)
     );
-}
+};
+/* I.S. : T sembarang */
+/* F.S. : Nilai T ditulis dg format HH.MM */
+/* Proses : menulis nilai setiap komponen T ke layar dalam format HH:MM:SS
+   tanpa karakter apa pun di depan atau belakangnya, termasuk spasi, enter,
+   dll.*/
 
 /* ***************************************************************** */
 /* KELOMPOK KONVERSI TERHADAP TYPE                                   */
 /* ***************************************************************** */
-long TIMEToDetik (Time T) {
-/* Diberikan sebuah Time, mengkonversi menjadi jumlah detik dari pukul 0:0:0 */
-/* Rumus : detik = 3600*HH + 60*MM + SS */
-/* Nilai maksimum = 3600*23+59*60+59 */
-    return 3600*Hour(T) + 60*Minute(T) + Second(T);
-}
+long TIMEToMenit(Time T){
+    return 60*Hour(T) + Minute(T) + 1440*Day(T);
+};
+/* Diberikan sebuah TIME, mengkonversi menjadi jumlah menit dari pukul 0:0:0 */
+/* Rumus : menit = 1440*DD + 60*HH + MM*/
+/* Nilai maksimum = 1440*29+23*60+59 */
 
-Time DetikToTIME (long N) {
-/* Mengirim  konversi detik ke Time */
-/* Catatan: Jika N >= 86400, maka harus dikonversi dulu menjadi jumlah detik yang
-   mewakili jumlah detik yang mungkin dalam 1 hari, yaitu dengan rumus:
-   N1 = N mod 86400, baru N1 dikonversi menjadi Time */
+Time MenitToTIME(long N){
     Time T;
     int HH;
     int MM;
-    int SS;
+    int DD;
 
-    // Jika jumlah detik >= 86400
-    if (N >= 86400)
+    // Jika jumlah menit >= 43200
+    if (N >= 43200)
     {
-        N %= 86400;
+        N %= 43200;
     }
-    // Jika jumlah detik < 0
+    // Jika jumlah menit < 0
     while (N < 0)
     {
-        N += 86400;
+        N += 43200;
     }
 
-    HH = N / 3600;
-    MM = (N % 3600) / 60;
-    SS = (N % 3600) % 60;
-    CreateTime(&T, HH, MM, SS);
+    DD = N / 1440;
+    HH = (N % 1440) / 60;
+    MM = (N % 1440) % 60;
+    CreateTime(&T, DD, HH, MM);
     return T;
-}
+};
+/* Mengirim  konversi menit ke TIME */
+/* Catatan: Jika N >= 43200, maka harus dikonversi dulu menjadi jumlah menit
+   yang mewakili jumlah menit yang mungkin dalam 1 hari, yaitu dengan rumus: N1
+   = N mod 86400, baru N1 dikonversi menjadi TIME */
 
 /* ***************************************************************** */
 /* KELOMPOK OPERASI TERHADAP TYPE                                    */
@@ -132,32 +131,31 @@ boolean TGT (Time T1, Time T2) {
 /* Mengirimkan true jika T1>T2, false jika tidak */
     return TIMEToDetik(T1) > TIMEToDetik(T2);
 }
-/* *** Operator aritmatika Time *** */
-Time NextDetik (Time T) {
-/* Mengirim 1 detik setelah T dalam bentuk Time */
-    return DetikToTIME(TIMEToDetik(T) + 1);
-}
-Time NextNDetik (Time T, int N) {
-/* Mengirim N detik setelah T dalam bentuk Time */
-    return DetikToTIME(TIMEToDetik(T) + N);
-}
-Time PrevDetik (Time T) {
-/* Mengirim 1 detik sebelum T dalam bentuk Time */
-    return DetikToTIME(TIMEToDetik(T) - 1);
-}
-Time PrevNDetik (Time T, int N) {
-/* Mengirim N detik sebelum T dalam bentuk Time */
-    return DetikToTIME(TIMEToDetik(T) - N);
-}
+/* *** Operator aritmatika TIME *** */
+Time NextMenit(Time T) {
+    return MenitToTIME(TIMEToMenit(T)+1); 
+};
+/* Mengirim 1 menit setelah T dalam bentuk TIME */
+Time NextNMenit(Time T, int N){
+    return MenitToTIME(TIMEToMenit(T)+N);
+};
+/* Mengirim N menit setelah T dalam bentuk TIME */
+Time PrevMenit(Time T){
+    return MenitToTIME(TIMEToMenit(T)-1);
+};
+/* Mengirim 1 menit sebelum T dalam bentuk TIME */
+Time PrevNMenit(Time T, int N){
+    return MenitToTIME(TIMEToMenit(T)-N);
+};
+/* Mengirim N menit sebelum T dalam bentuk TIME */
 /* *** Kelompok Operator Aritmetika *** */
-long Durasi (Time TAw, Time TAkh) {
-/* Mengirim TAkh-TAw dlm Detik, dengan kalkulasi */
-/* Jika TAw > TAkh, maka TAkh adalah 1 hari setelah TAw */
-    long detikTAw = TIMEToDetik(TAw);
-    long detikTAkh = TIMEToDetik(TAkh);
-    if (TGT(TAw, TAkh))
-    {
-        detikTAkh += 86400;
+long Durasi(Time TAw, Time TAkh) { 
+    long detikTAw = TIMEToMenit(TAw);
+    long detikTAkh = TIMEToMenit(TAkh);
+    if (TGT(TAw, TAkh)){
+        detikTAkh += 43200;
     }
     return detikTAkh - detikTAw;
-}
+};
+/* Mengirim TAkh-TAw dlm menit, dengan kalkulasi */
+/* Jika TAw > TAkh, maka TAkh adalah 1 hari setelah TAw */
