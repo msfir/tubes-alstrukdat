8d41e482dc30e586a073b8a8cbe99c98ae7c9adb
AustinPardosi <88770149+AustinPardosi@users.noreply.github.com>
2022-10-28 09:42:09 +0700

    Update Simulator

--- header/adt/simulator.h
+++ header/adt/simulator.h
@@ -6,12 +6,15 @@
 
 #include "../boolean.h"
 #include "point.h"
+#include "string.h"
 #include "prioqueuetime.h"
 #include "time.h"
+// #include "notification.h"
 
 /* *** Definisi TYPE Simulator *** */
 typedef struct {
-    char username[0xff];
+    string username[0xff];
+    Time time;
     Point location;
     PrioQueueTime inventory;
 } Simulator;
@@ -20,7 +23,30 @@ typedef struct {
 // clang-format off
 #define Username(I)  (I).username
 #define Location(I)  (I).location
-#define inventory(I) (I).inventory
+#define Inventory(I) (I).inventory
+#define Time(I) (I).time
 // clang-format on
+
+/* *** Konstruktor: Membentuk sebuah Simulator dari komponen-komponennya *** */
+void CreateSimulator (Simulator *x);
+/* I.S. x sembarang
+   F.S. x terdefinisi*/
+
+/* ***************************************************************** */
+/* KELOMPOK OPERASI TERHADAP TYPE                                    */
+/* ***************************************************************** */
+// boolean IsTherePlace (Simulator *x);
+/* Mengirim true jika ada tempat (M, T, F, C, B) di sekitar Simulator */
+
+// boolean IsThereWall (Simulator *x);
+/* Mengirim true jika ada dinding di sekitar Simulator */
+
+// void SimulatorMove (Simulator *x, string direct);
+/* I.S. Posisi Simulator terdefinisi
+   F.S. Simulator berubah posisi*/
+
+// void SimulatorCommand (Simulator *x, string command);
+/* I.S. Simulator terdefinisi
+   F.S. Simulator melakukan command dengan tipe data string*/
 
 #endif
