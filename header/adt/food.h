3de5b355c910d858bcded685b73ea4a1e0fa4d29
sofyanfirdaus <13521083@std.stei.itb.ac.id>
2022-11-19 02:30:55 +0700

    feat: bonus 2, waktu pengolahan makanan

--- header/adt/food.h
+++ header/adt/food.h
@@ -32,6 +32,7 @@ typedef struct {
     enum Action action_loc; /* aksi yang harus dilakukan pada food */
     Time delivery_time; /* lama pengiriman food */
     Size size; /* ukuran food */
+    Time processing_time; // waktu pengolahan makanan
 } Food;
 
 /********************************/
@@ -49,9 +50,11 @@ typedef struct {
 #define DeliveryTime(F)   (F).delivery_time
 //return size
 #define Size(F)           (F).size
+//return processing_time
+#define ProcTime(F)       (F).processing_time
 
 /* *** Konstruktor: Membentuk food dengan berbagai komponen sebagai properties-nya *** */
-void CreateFood(Food* F, int id, String name, Time expiration_time, enum Action action_loc, Time delivery_time, Size size);
+void CreateFood(Food* F, int id, String name, Time expiration_time, enum Action action_loc, Time delivery_time, Size size, Time processing_time);
 /* I.S. semua parameter terdefinisi */
 /* F.S. Terbentuk food dengan properties terdefinisi */
 
