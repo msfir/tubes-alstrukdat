ea937422aaa0ddf4e27f58b835e43a7d5d6d6c80
sofyanfirdaus <13521083@std.stei.itb.ac.id>
2022-11-18 21:22:17 +0700

    feat: handle bonus 3 properly using set

---
+++ src/tests/test_set.c
@@ -0,0 +1,34 @@
+#include "set.h"
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+    Set a;
+    CreateEmptySet(&a);
+    set_add(&a, 3);
+    set_add(&a, 13);
+    set_add(&a, 42);
+    set_add(&a, 60);
+    set_add(&a, 4);
+    set_add(&a, 6);
+    set_add(&a, 9);
+    set_remove(&a, 30);
+    set_remove(&a, 42);
+    set_add(&a, 60);
+    set_add(&a, 4);
+    assert(set_length(a) == 6);
+    Set b;
+    CreateEmptySet(&b);
+    set_add(&b, 42);
+    set_add(&b, 60);
+    set_add(&b, 4);
+    set_add(&b, 54);
+    set_add(&b, 22);
+    assert(set_length(b) == 5);
+    Set c = set_intersection(a, b);
+    assert(set_length(c) == 2);
+    c = set_union(a, b);
+    assert(set_length(c) == 9);
+    printf("All tests passed.\n");
+    return 0;
+}
