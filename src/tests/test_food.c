40c121de9988500a13c6adfe1f1dd3475f8344ea
fakihap <fakih.anugerah@gmail.com>
2022-11-20 00:37:39 +0700

    edit: test driver ADT food, resep, tree
    + hapus comment di main.c

--- src/tests/test_food.c
+++ src/tests/test_food.c
@@ -5,12 +5,21 @@
 
 
 int main(){ 
+    printf("--- Driver Test Food ---");
+    printf("\n")   ;
+    printf("\n")   ;
     Time contohTime; CreateTime(&contohTime, 1, 2, 3);
     Size size; size.width = 1; size.height = 2;
     Food ayam; 
     CreateFood(&ayam, 420, StringFrom("ayam rasa bebek"), contohTime, MIX, PrevNMenit(contohTime, 3600), size, MenitToTIME(0));
     
+
     DisplayFood(ayam);
 
+    printf("\n")   ;
+    printf("\n")   ;
+    printf("--- Driver Test Food ---");
+    printf("\n")   ;
+    printf("\n")   ;
     return 0;
 }
---
+++ src/tests/test_resep.c
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+#include "food.h"
+#include "time.h"
+#include "node.h"
+#include "tree.h"
+#include "cstring.h"
+
+#include "resep.h"
+
+
+int main(){ 
+    printf("--- Driver Test Resep ---");
+    printf("\n")   ;
+    printf("\n")   ;
+
+    // contohFood
+    Time contohTime; CreateTime(&contohTime, 1, 2, 3);
+    Size size; size.width = 1; size.height = 2;
+    Food ayam; 
+    CreateFood(&ayam, 420, StringFrom("ayam rasa bebek"), contohTime, MIX, PrevNMenit(contohTime, 3600), size, MenitToTIME(0));
+
+    FoodList foodList;
+    CreateFoodList(&foodList);
+    addFood(&foodList, ayam);
+
+    // resep
+    Resep resep;
+    ResepList resepList;
+    Address foodNode = newNode(Id(ayam));
+    Tree foodTree;
+    CreateTree(&foodTree, foodNode);
+    
+
+    CreateResep(&resep, foodTree);
+    ResepListElmt(resepList, 0) = resep;
+
+    
+    printCookbook(resepList, foodList);
+
+    printf("\n")   ;
+    printf("\n")   ;
+    printf("--- Driver Test Resep ---");
+    printf("\n")   ;
+    printf("\n")   ;
+    return 0;
+}
